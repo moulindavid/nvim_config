67d97303ef3d897998fb30e999b8ccba43f6e623
moulindavid <moulin.david@live.fr>
2023-04-23 16:50:38 +0200

    feat: more updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,7 +30,7 @@ lsp.format_on_save({
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-q>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
     ['<Enter>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
